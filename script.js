da989a1eab7ea32618f4199d0e12f15aaca348bc
INCON! <samuelcaetanosr79@gmail.com>
2026-09-12 11:11:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function tema() {
     let tema = document.body
     tema.classList.toggle("dark-mode")
     let bgBtn = document.body.getElementsById("botao-tema")
-    bgBtn.style.backgroundColor = 'rgba(26, 117, 159, 1)'
+    bgBtn.style.backgroundColor.toggle = rgba(26, 117, 159, 1)
 }
